211ac1940a3ba3c5bc85cc28a6e0a4ea7b7151f9
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2024-01-31 18:07:32 +0300

    2. LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -28,20 +28,29 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """_summary_
+    """Inherits from BaseCaching and implements a caching system using
+    LIFO eviction policy
     """
 
     def __init__(self):
-        """_summary_
+        """Constructor for LIFOCache class
+        Initializes the LIFOCache instance by calling the constructor of the
+        parent class BaseCaching.
         """
         super().__init__()
 
     def put(self, key, item):
-        """_summary_
+        """Inserts a key-value pair into the cache
 
         Args:
-                        key (_type_): _description_
-                        item (_type_): _description_
+            key: The key to be inserted into the cache.
+            item: The value corresponding to the key to be stored in the cache.
+
+        If key or item is None, this method should not do anything.
+        If the number of items in self.cache_data exceeds
+        BaseCaching.MAX_ITEMS,
+        the last item put in cache (LIFO algorithm) is discarded, and a message
+        indicating the discarded key is printed.
         """
         if key is None or item is None:
             pass
@@ -55,10 +64,14 @@ class LIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """return the value in self.cache_data linked to key
+        """Retrieves the value associated with the given key from the cache
 
         Args:
-                        key (_type_): _description_
+            key: The key for which the value needs to be retrieved.
+
+        Returns:
+            The value associated with the given key if key is present in the
+            cache, else returns None.
         """
         if key is None or key not in self.cache_data.keys():
             return None
